c5a000ee3788aa51e837465cbe8e3a120b9e1265
Isa <88134377+disaMartinez@users.noreply.github.com>
2023-11-17 18:54:29 +0100

    fucciona toro, pero feo

--- hamman.js
+++ hamman.js
@@ -5,18 +5,23 @@ let paraules = [
     'Radiant', 'Quasar', 'Serendipity', 'Saffron', 'Umbrella', 'Velocity', 'Whimsical', 'Cascade', 'Nebula', 'Zenith', 'Octopus'
 ];
 
+let loses = 0;
+let wins = 0;
+
 class Player {
     name;
     word;               //La paraula a esbrinar
     guessed;            //El progress que porta ???? ????
+    haGanado;
     lives;              //Els intents que li queden
     letters;            //Les lletres que ha dit
 
     constructor() {
         this.name = "";
         this.word = "";
-        this.guessed = "";
-        this.lives = 9;
+        this.haGanado = false;
+        this.haGanadoGuessWord = false;
+        this.lives = 8;
         this.letters = [];
     }
 
@@ -27,6 +32,11 @@ class Player {
     wordToGuess(word, guessed) {
         this.word = word;
         this.guessed = guessed;
+    }
+
+    getHaGanado(){
+        this.haGanado = NewPlayer.guessed.toString().replaceAll(',', '') == NewPlayer.word.toLowerCase();
+        return this.haGanado;
     }
 }
 
@@ -60,7 +70,6 @@ function load() {
     });
 
 }
-
 
 function startGame() {
 
@@ -101,16 +110,41 @@ function getAllIndexes(arr, val) {
     return indexes;
 }
 
+function guessWord(){
+    let paraula = document.getElementById('guessWord').value;
 
-function guess(letra, form) {
-    //console.log(form)
-    //Funcion para mirar si la letra o la palabra que ha entrado esiste
-    var posicionesDeLaLetra = getAllIndexes(NewPlayer.word, letra.textContent);
     //Miramos si el guess no esta vacio
-    /*if (letra.trim() === '') {
+    if (paraula.trim() === '') {
         alert('Por favor, ingresa una palabra.');
         return;
-    }*/
+    }
+
+    if(paraula.trim().length != NewPlayer.word.length){
+        alert('Por favor, ingresa una palabra de largada: ' + NewPlayer.word.length);
+        return;
+    }
+
+    if(paraula.toLowerCase() == NewPlayer.word.toLowerCase()){
+        console.log("Funciona?");
+        NewPlayer.haGanadoGuessWord = true;
+        console.log(NewPlayer.haGanado);
+    }
+    else{
+        NewPlayer.lives--;
+    }
+
+    updateUI();
+
+}
+
+function guess(letra) {
+    
+    //Funcion para mirar si la letra o la palabra que ha entrado esiste
+    var posicionesDeLaLetra = getAllIndexes(NewPlayer.word.toUpperCase(), letra.textContent);
+
+    letra.disabled = true;
+    NewPlayer.letters.push(letra.textContent.toLowerCase());    
+
     if (posicionesDeLaLetra.length == 0) {
         //Ha fallado
         //Quitar vida
@@ -122,7 +156,6 @@ function guess(letra, form) {
         //Bloquear letra
         letra.disabled = true;
         //Cambiar imagen
-        actualizarImagenAhorcado();
     }
     else {
         //Ha asertaro
@@ -137,71 +170,67 @@ function guess(letra, form) {
         updateUI();
     }
 
-    // Eliminar letra
-    if (form) {
-        let text = document.getElementById('guessWord').value.split("");
-        text.forEach(element => {
-            //console.log(document.getElementById(element.toUpperCase()))
-            document.getElementById(element.toUpperCase()).disabled = true;
-            NewPlayer.letters.push(element);
-        });
-        
-    } else {
-        letra.disabled = true;
-        NewPlayer.letters.push(letra.textContent.toLowerCase());
-    }
-
     // Cambiar el NewPlayer guessed y letters
-    NewPlayer.guessedString = NewPlayer.guessed.join('');
+    //NewPlayer.guessedString = NewPlayer.guessed.join('');
     //NewPlayer.letters.push(letra.textContent.toLowerCase());
     // Añadir la letra que ha dicho
     NewPlayer.letters.push(letra);
+    
 
-
-    //Mirar si ha gastado todas sus vidas
-    if (NewPlayer.lives === 0) {
-        // Morision
-        // Cambiar la imagen a la de perder
-        actualizarImagenAhorcado()
-        // (Aquí puedes realizar alguna acción para indicar que el jugador ha perdido)
-        alert('¡Lo siento, has perdido! La palabra correcta era: ' + NewPlayer.word);
-        reiniciarJuego();
-    }
     // Actualizar la interfaz de usuario
     updateUI();
 }
 
 // Función para actualizar la imagen del ahorcado
 function actualizarImagenAhorcado() {
+
     // imagen 
     var imagenAhorcado = document.getElementById("imgTauler");
+
     if (imagenAhorcado) {
         // Verificar si el jugador ha perdido
         if (NewPlayer.lives === 0) {
             // Cambiar la imagen a la de perder
-            imagenAhorcado.src = "jamman_lose.png";
-        } else if (NewPlayer.lives === -1) { //Posar win condition
+            imagenAhorcado.src = "./img/jamman_lose.png";
+        } else if (NewPlayer.getHaGanado() || NewPlayer.haGanadoGuessWord) { //Posar win condition
             // Cambiar la imagen a la de ganar
-            imagenAhorcado.src = "jamman_win.png";
+            imagenAhorcado.src = "./img/jamman_win.png";
         } else {
             // Actualiza la imagen según las vidas que faltan
-            imagenAhorcado.src = `img/jamman_${NewPlayer.lives}.png`;
+            imagenAhorcado.src = `./img/jamman_${NewPlayer.lives}.png`;
         }
     }
-
 }
 
 // Función para reiniciar el juego
 function reiniciarJuego() {
+
+    NewPlayer = new Player();
+
+    document.getElementById("btnReiniciar").style.display = "none";
+    document.getElementById("btnSortir").style.display = "none";
+
     // Recargar la página para reiniciar el juego
     location.reload();
 }
 
-function startHamman() {
+function startHamman() {   
+
+    NewPlayer = new Player();
 
     //Escojer una palabra de la lista y añadirla al jugador como word
     var word = paraules[Math.floor(Math.random() * paraules.length)];
     console.log(word);
+
+    document.getElementById("btnReiniciar").style.display = "none";
+    document.getElementById("btnSortir").style.display = "none";
+
+    
+    var abecedario = document.getElementsByClassName("lletra-btn");
+
+    Array.from(abecedario).forEach(element => {
+        element.disabled = false;
+    });
 
     //Cargar las _ _ _ a wordsToGuess  
     var guessed = []
@@ -218,23 +247,39 @@ function startHamman() {
     //Poner los _ en el tablero
     var puntitos = document.getElementById("wordsToGuess");
     puntitos.innerText = guessed.toString().replaceAll(',', ' ',);
+
+    actualizarImagenAhorcado();
 }
 
 // Función para obtener y mostrar las estadísticas almacenadas en localStorage
 function showStats() {
-    var won = localStorage.getItem('won') || 0;
-    var lost = localStorage.getItem('lost') || 0;
 
-    document.getElementById('won').textContent = won;
-    document.getElementById('lost').textContent = lost;
+    if(NewPlayer.lives == 0){
+        loses++;
+    }
+    else if( NewPlayer.haGanadoGuessWord || NewPlayer.getHaGanado()){
+        wins++;
+    }
+
+    document.getElementById('won').textContent = wins;
+    document.getElementById('lost').textContent = loses;
 }
 
 function updateUI() {
-    // Actualizar estadísticas
+
+    console.log(NewPlayer.haGanado);
+
+    //Mirar si ha gastado todas sus vidas
+    if (NewPlayer.lives == 0 || NewPlayer.haGanadoGuessWord || NewPlayer.getHaGanado()) {
+        document.getElementById("btnReiniciar").style.display = "inline-block";
+        document.getElementById("btnSortir").style.display = "inline-block";
+    }
+
+    actualizarImagenAhorcado();
+    
     showStats();
 
     // Actualizar el tablero de letras adivinadas
     var puntitos = document.getElementById("wordsToGuess");
-    puntitos.innerText = NewPlayer.guessedString.replaceAll('_', ' ');
-
+    puntitos.innerText = NewPlayer.guessed.toString().replaceAll(',', ' ',);
 }
